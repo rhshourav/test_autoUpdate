03532bbb0f288aa8632fe5e8cf8f4a1c6222de4f
rhshourav <rhshourav02@gmail.com>
2024-07-25 16:57:25 +0600

    updated

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-// index.js
-const Mustache = require('mustache');
 const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
-let DATA = {
-  name: 'Thomas',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'Europe/Stockholm',
-  }),
-};
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
-}
-generateReadMe();
+const path = require('path');
+const mustache = require('mustache');
+
+const readmePath = path.join(__dirname, 'README.md');
+const template = `
+# My Project
+
+This README was last updated at {{date}}.
+`;
+
+const output = mustache.render(template, { date: new Date().toISOString() });
+
+fs.writeFileSync(readmePath, output, 'utf8');
